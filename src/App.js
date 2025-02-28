5c11fc59dc6c33cf195a55aea1ce2ddef4725a08
kecci <abyanjksatu@gmail.com>
2025-03-01 00:49:06 +0700

    feature/adjust-new-api-coinstats (#1)
    Co-authored-by: Kecci Kun <abyan.kecci@stockbit.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import "./App.css";
-import { useEffect, useState } from "react";
 import Axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
 import Coin from "./components/Coin";
 
 function App() {
@@ -8,11 +8,27 @@ function App() {
   const [search, setSearch] = useState("");
 
   useEffect(() => {
-    Axios.get("https://api.coinstats.app/public/v1/coins?skip=0").then(
-      (response) => {
-        setListOfCoins(response.data.coins);
-      }
-    );
+    // This API is deprecated and will be disabled by Oct 31 2023, to use the new version please go to https://openapi.coinstats.app .
+    // Axios.get("https://api.coinstats.app/public/v1/coins?skip=0").then(
+    //   (response) => {
+    //     setListOfCoins(response.data.coins);
+    //   }
+    // );
+    //
+    // This is a new one from openapi.coinstats.app, this have a monthly API Credit limit 1,000,000/month
+    // To see the credit limit, please checkout this: https://openapi.coinstats.app/
+    Axios.get("https://openapiv1.coinstats.app/coins", {
+      headers: {
+        "X-API-KEY": "jnNuzZKueK5/PuI6oKXLXZOzJpSkO7K1F1t3RPVjMv4=",
+        accept: "application/json",
+      },
+    }).then((response) => {
+      // Deprecated response of field "coins"
+      // setListOfCoins(response.data.coins);
+      //
+      // Instead use the new response by field "result"
+      setListOfCoins(response.data.result);
+    });
   }, []);
 
   const filteredCoins = listOfCoins.filter((coin) => {
